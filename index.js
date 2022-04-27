2c12f34de00945e98fab6a4b058737be1452fc19
Brethan <80044242+Brethan@users.noreply.github.com>
2022-04-27 01:55:30 -0400

    Removed intent console log

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const intents = [];
 for (const intent in Intents.FLAGS) {
     intents.push(intent);
 }
-console.log(intents);
+
 const client = new Client(intents);
 
 client.login(process.env.TOKEN);
