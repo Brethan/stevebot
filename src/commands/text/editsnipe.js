744022ff2ec2c43e7c1127fb51a424b8ae9cd157
Brethan <80044242+Brethan@users.noreply.github.com>
2022-04-27 02:00:51 -0400

    Create files for various commands for later

---
+++ src/commands/text/editsnipe.js
@@ -0,0 +1,27 @@
+const Command = require('../Command.js');
+const discord = require('discord.js');
+
+module.exports = class  extends Command {
+    constructor(client) {
+        super(client, {
+            "name": "",
+            "alias": "",
+            "description": "",
+            "permissions": ["PERMISSIONS"],
+            "args": true,
+            "minArgs": 1,
+            "expectedArgs": ["args", ],
+            "usage": ['s. '],
+            "ownerCommand": false,
+            "disabled": false,
+        })
+    }
+    /**
+     * @param {discord.Message} message
+     * @param {string[]} args
+     */
+    execute(message, args) {
+
+    }
+
+}
