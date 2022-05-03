ab6da45083c717eb6634ffa17b184b50781d59ae
Brethan <80044242+Brethan@users.noreply.github.com>
2022-05-03 16:53:27 -0400

    Added command and event collections to the client class

--- src/Client.js
+++ src/Client.js
@@ -1,5 +1,8 @@
-const { writeFileSync } = require("fs");
+const { writeFileSync, readdir, readdirSync } = require("fs");
 const discord = require("discord.js");
+const { join } = require("path");
+const Command = require("./commands/Command");
+const Event = require("./events/Event");
 
 module.exports = class Client extends discord.Client {
 
@@ -7,8 +10,30 @@ module.exports = class Client extends discord.Client {
         super({ intents: intents });
 
         this.config = require("../config.json");
+
+        /**
+         * @type {discord.Collection<string, Command>}
+         */
+        this.commands = new discord.Collection()
+
+        /**
+         * @type {discord.Collection<string, Command>}
+         */
+        this.aliases = new discord.Collection();
+
+        /**
+         * @type {discord.Collection<string, Event>}
+         */
+        this.events = new discord.Collection();
+        
+        this.initHandlers();
     }
 
+    initHandlers() {
+        readdirSync(join(__dirname, "handlers"))
+            .filter(file => file.endsWith("handler.js"))
+            .forEach(handler => require("./handlers/" + handler)(this, discord));
+    }
 
     get prefix() {
         return this.config.prefix;
