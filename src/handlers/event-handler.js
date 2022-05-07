d44f9da5b00ae3779bd7e38838d2c2bb398b2a9e
Brethan <80044242+Brethan@users.noreply.github.com>
2022-05-07 00:24:33 -0400

    Implemented the event loader

--- src/handlers/event-handler.js
+++ src/handlers/event-handler.js
@@ -1,3 +1,27 @@
-module.exports = (client, discord) => {
-    
+const { readdirSync } = require("fs");
+const { resolve } = require("path");
+const Client = require("../Client");
+
+/**
+ * 
+ * @param {Client} client 
+ */
+module.exports = (client) => {
+
+    /**
+     * 
+     * @param {string} dir 
+     */
+    const loadEvents = (dir) => {
+        const event_basePath = resolve("./src", "events", dir);
+        const eventFiles = readdirSync(event_basePath).filter(file => file.endsWith(".js"));
+
+        for (const eventSrc of eventFiles) {
+            const event = require(resolve(event_basePath, eventSrc));
+            const eventName = eventSrc.split(".")[0];
+            client[event.once ? "once" : "on"](eventName, event.bind(null, client));
+        }
+    }
+
+    ["client", "guild"].forEach(e => loadEvents(e));
 }
