24aea0f79c59fb095b8ee6272d44a771f9a91c2d
Brethan <80044242+Brethan@users.noreply.github.com>
2022-09-18 16:27:55 -0400

    Help command stub, ready for implementation

--- src/commands/util/help.js
+++ src/commands/util/help.js
@@ -9,17 +9,39 @@ module.exports = class Help extends Command {
             "permissions": ["SEND_MESSAGES"],
             "args": false,
             "usage": ['s.help ', "s.help `commandName`"],
+            autoclear: 5000
         })
     }
+
     /**
      * @param {discord.Message} message
      * @param {string[]} args
+     * @return {string | discord.MessageEmbed}
      */
-    execute(message, args) {
-
+    async execute(message, args) {
+        return "This command has not been implemented yet!";
     }
 
-    help(message, commandName) {
-        message.channel.send("the help command has not been implemented yet.")
+    /**
+     * Sends a message to the channel containing the usage of
+     * the command with the name commandName 
+     * 
+     * @param {discord.Message} message 
+     * @param {string} commandName Name of the command to generate usage for
+     * @return {string | discord.MessageEmbed}
+     */
+    async defaultHelp(message, commandName) {
+        return "The help command has not been implemented yet!";
+    }
+
+    /**
+     * Sends a message to the channel containing instructions 
+     * on how to use this command
+     * 
+     * @param {discord.Message} message
+     * @return {string | discord.MessageEmbed}
+     */
+    help(message) {
+        return "The help command has not been implemented yet!";
     }
 }
