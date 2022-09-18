2faeb60b8decb8090e32b738caadf0d6a5b709bb
Brethan <80044242+Brethan@users.noreply.github.com>
2022-09-18 16:28:24 -0400

    messageCreate event listener stub, ready for implementation

--- src/events/guild/messageCreate.js
+++ src/events/guild/messageCreate.js
@@ -18,6 +18,8 @@ module.exports = class MessageCreate extends Event {
      * @param {Message} message 
      */
     async messageCreate(client, message) {
+        if (message.author.bot) return;
+        if (message.content.startsWith(client.prefix)) return
         console.log(message?.content);
     }
 }
