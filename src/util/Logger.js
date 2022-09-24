a05c44a9c0703a2696f519d644bacb0dcf0e425c
Brethan <80044242+Brethan@users.noreply.github.com>
2022-09-24 17:10:57 -0400

    Logger - ESLintified and implemented event adding and removing

--- src/util/Logger.js
+++ src/util/Logger.js
@@ -1,64 +1,101 @@
+// eslint-disable-next-line no-unused-vars
 const SteveClient = require("../Client");
 
 module.exports = class Logger {
-    /**
-     * 
-     * @param {SteveClient} client 
-     */
-    constructor(client) {
-        /** @type {SteveClient} */
-        this.client = client;
-        this.logSettings = client.config.log_settings;
-    }
+	/**
+	 *
+	 * @param {SteveClient} client
+	 */
+	constructor(client) {
+		/** @type {SteveClient} */
+		this.client = client;
+		this.logSettings = client.config.log_settings;
+	}
 
-    get events() {
-        return this.logSettings.events;
-    }
+	get events() {
+		return this.logSettings.events;
+	}
 
-    get members() {
-        return this.logSettings.members;
-    }
-    
-    get loggingChannel() {
-        return this.logSettings.channel;
-    }
+	get members() {
+		return this.logSettings.members;
+	}
 
-    set events(events) {
-        this.logSettings.events = events;
-        this.client.overwriteConfig();
-    }
+	get loggingChannel() {
+		return this.logSettings.channel;
+	}
 
-    set members(members) {
-        this.logSettings.members = members;
-        this.client.overwriteConfig();
-    }
+	set events(events) {
+		this.logSettings.events = events;
+		this.client.overwriteConfig();
+	}
 
-    set loggingChannel(channel) {
-        this.logSettings.channel = this.client.resolveIdFromMention(channel);
-        this.client.overwriteConfig();
-    }
+	set members(members) {
+		this.logSettings.members = members;
+		this.client.overwriteConfig();
+	}
 
-    /**
-     * 
-     * @param {string | string[]} members 
-     */
-    addMembersToLog(members) {
-        if (typeof members === "string") {
-            members = [members];
-        }
-        
-        this.members = Array.from(new Set([...this.members].concat(members)));
-    }
+	set loggingChannel(channel) {
+		this.logSettings.channel = this.client.resolveIdFromMention(channel);
+		this.client.overwriteConfig();
+	}
 
-    /**
-     * 
-     * @param {string | string[]} members 
-     */
-    removeMembersFromLog(members) {
-        if (typeof members === "string") {
-            members = [members];
-        }
+	/**
+	 *
+	 * @param {string | string[]} toAdd
+	 * @param {"members" | "events"} method
+	 */
+	addToLog(toAdd, method) {
+		if (typeof toAdd === "string") {
+			toAdd = [toAdd];
+		}
 
-        this.members = this.members.filter(id => !members.includes(id));
-    }
-}
+		this[method] = Array.from(new Set([...this[method]].concat(toAdd)));
+	}
+
+	/**
+	 *
+	 * @param {string | string[]} members
+	 */
+	addMembersToLog(members) {
+		this.addToLog(members, "members");
+	}
+
+	/**
+	 *
+	 * @param {string | string[]} events
+	 */
+	addEventsToLog(events) {
+		this.addToLog(events, "events");
+	}
+
+	/**
+	 *
+	 * @param {string | string[]} toRemove
+	 * @param {"events" | "members"} method
+	 */
+	removeFromLog(toRemove, method) {
+		if (typeof toRemove === "string") {
+			toRemove = [toRemove];
+		}
+
+		this[method] = this[method].filter(str => !toRemove.includes(str));
+	}
+
+	/**
+	 *
+	 * @param {string | string[]} members
+	 */
+	removeMembersFromLog(members) {
+		this.removeFromLog(members, "members");
+	}
+
+	/**
+	 *
+	 * @param {string | string[]} events
+	 */
+	removeEventsFromLog(events) {
+		this.removeFromLog(events, "events");
+	}
+
+
+};
