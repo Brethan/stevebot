9cfb28fcf63fff864f80cb5469702af08e9abc10
Brethan <80044242+Brethan@users.noreply.github.com>
2022-10-24 01:29:52 -0400

    List of courses will be updated once a week

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -3,6 +3,7 @@
 // eslint-disable-next-line no-unused-vars
 const SteveClient = require("../../Client");
 const Event = require("../Event");
+const fetchCourses = require("../../util/course-fetcher.js");
 
 module.exports = class Ready extends Event {
 	constructor() {
@@ -13,9 +14,18 @@ module.exports = class Ready extends Event {
 	 * @param {SteveClient} client
 	 */
 	async ready(client) {
+		const week = (1000 * 60 * 60) * 24 * 7;
+
+		setInterval(async () => {
+			console.log("INFO: Updating list of courses.");
+			client.courses = await fetchCourses();
+			client.overwriteCourses();
+		}, week);
+
 		process.stdout.write(`\n\rNOTICE: ${client.user?.tag} is ready and waiting\n`);
 		client.user?.setActivity({
 			name: "stevebotV5 in development :o",
 		});
+
 	}
 };
