7e582776f0e3110cdcf74ef980af2255a5960bf4
Brethan <80044242+Brethan@users.noreply.github.com>
2022-10-25 04:15:08 -0400

    Command - course.js: implementation complete - now in maintenance

--- src/commands/util/course.js
+++ src/commands/util/course.js
@@ -1,6 +1,6 @@
 const Command = require('../Command.js');
 // eslint-disable-next-line no-unused-vars
-const { AutocompleteInteraction, ChatInputCommandInteraction, SlashCommandBooleanOption } = require('discord.js');
+const { AutocompleteInteraction, ChatInputCommandInteraction, SlashCommandBooleanOption, EmbedBuilder } = require('discord.js');
 const courseInfo = require('../../util/course-info.js');
 
 module.exports = class Course extends Command {
@@ -10,11 +10,12 @@ module.exports = class Course extends Command {
 			"description": "Command for testing out them buttons",
 			"slashOnly": true,
 			"permissions": ["Administrator"],
-			"usage": ['s.button'],
 			"owner": true,
 			autoclear: 8_000,
 		});
 
+		this.subcommands = ["info", "exams"];
+		this.stringOptions = ["department", "number", "section"];
 		/**
 		 * Instead of doing some kind of modular thing in the constructor,
 		 * there should be a function that creates a slash command rather than them
@@ -29,8 +30,9 @@ module.exports = class Course extends Command {
 	 */
 	createSlashCommand(_args0) {
 
-		const departmentOption = this.createCmdStringOption("department");
-		const codeOption = this.createCmdStringOption("course_code");
+		const departmentOption = this.createCmdStringOption(this.stringOptions[0]);
+		const codeOption = this.createCmdStringOption(this.stringOptions[1]);
+		const sectionOption = this.createCmdStringOption(this.stringOptions[2]);
 		const invisibleOption = new SlashCommandBooleanOption()
 			.setName("visible")
 			.setDescription("Make the command visible to other users")
@@ -38,8 +40,8 @@ module.exports = class Course extends Command {
 
 		const options = [departmentOption, codeOption, invisibleOption];
 		const slash = super.createSlashCommand(false)
-			.addSubcommand(this.createSubCommand("exam_schedule", null, ...options))
-			.addSubcommand(this.createSubCommand("info", null, ...options));
+			.addSubcommand(this.createSubCommand(this.subcommands[0], null, ...options))
+			.addSubcommand(this.createSubCommand(this.subcommands[1], null, ...options, sectionOption));
 
 		return slash;
 	}
@@ -62,19 +64,59 @@ module.exports = class Course extends Command {
 	}
 
 	/**
+	 * @typedef examObj
+	 * @property {string} startDate
+	 * @property {string} endDate
+	 * @property {string} length
+	 * @property {string} startTime
+	 * @property {string} endTime
+	 * @property {string} loc
+	 * @property {string} type
+	 */
+
+	/**
+	 *
+	 * @param {string} dept
+	 * @param {string} code
+	 */
+	examInfo(dept, code, section) {
+		/** @type {examObj} */
+		const exam = this.client.exams[dept][code][section];
+		return new EmbedBuilder()
+			.setTitle(`Exam Schedule`)
+			.setDescription(`Exam details for **${dept} ${code} - ${section}**`)
+			.setColor("Green")
+			.addFields(
+				{ name: "Start Date", value: exam.startDate, inline: true },
+				{ name: "End Date", value: exam.endDate, inline: true },
+				{ name: "Exam Duration", value: exam.length, inline: true },
+				{ name: "Type", value: exam.type, inline: true },
+				{ name: "Start Time", value: exam.startTime, inline: true },
+				{ name: "End Time", value: exam.endTime, inline: true },
+				{ name: "Location", value: exam.loc, inline: false },
+			);
+	}
+
+	/**
 	 *
 	 * @param {ChatInputCommandInteraction} interaction
 	 */
 	async handleChatInputCommand(interaction) {
 		const sub = interaction.options.getSubcommand();
-		const dept = interaction.options.getString("department", true);
-		const code = interaction.options.getString("course_code", true);
+		const dept = interaction.options.getString(this.stringOptions[0], true);
+		const code = interaction.options.getString(this.stringOptions[1], true);
 		const visible = interaction.options.getBoolean("visible", false);
 
-		if (sub === "info")
-			interaction.reply({ embeds: [await courseInfo(dept, code)], ephemeral: !visible });
-		else if (sub === "exam_schedule")
-			interaction.reply({ content: "Exam schedule command is in development!", ephemeral: !visible });
+		if (interaction.replied)
+			return;
+
+		if (sub === this.subcommands[0])
+			// eslint-disable-next-line curly
+			await interaction.reply({ embeds: [await courseInfo(dept, code)], ephemeral: !visible });
+		else if (sub === this.subcommands[1]) {
+			const section = interaction.options.getString(this.stringOptions[2], true);
+			await interaction.reply({ embeds: [this.examInfo(dept, code, section)], ephemeral: !visible });
+		}
 
 		return;
 	}
@@ -84,7 +126,10 @@ module.exports = class Course extends Command {
 	 * @param {AutocompleteInteraction} interaction
 	 */
 	async handleAutoComplete(interaction) {
-		const deptArr = Object.keys(this.client.courses);
+		const sub = interaction.options.getSubcommand(true);
+		const collection = (sub === this.subcommands[0]) ? this.client.courses : this.client.exams;
+
+		const deptArr = Object.keys(collection);
 		const foc = interaction.options.getFocused(true);
 
 		const respond = async (/** @type {string[]} */ arr) => {
@@ -93,14 +138,23 @@ module.exports = class Course extends Command {
 			await interaction.respond(limited.map(str => ({ name: str, value: str })));
 		};
 
-		if (foc.name === "department") {
-			respond(deptArr);
-		} else if (foc.name === "course_code") {
-			const deptVal = interaction.options.getString("department", true);
-			if (!this.client.courses[deptVal])
+		if (foc.name === this.stringOptions[0]) {
+			await respond(deptArr);
+		} else if (foc.name === this.stringOptions[1]) {
+			const deptVal = interaction.options.getString(this.stringOptions[0], true);
+			const codeArr = (sub === this.subcommands[0]) ? this.client.courses[deptVal] : Object.keys(this.client.exams[deptVal]);
+			if (!codeArr)
 				return;
 
-			respond(this.client.courses[deptVal]);
+			await respond(codeArr);
+		} else if (foc.name === this.stringOptions[2]) {
+			const deptVal = interaction.options.getString(this.stringOptions[0], true);
+			const codeVal = interaction.options.getString(this.stringOptions[1], true);
+			const sectionArr = this.client.exams[deptVal][codeVal].sections;
+			if (!sectionArr)
+				return;
+
+			await respond(sectionArr);
 		}
 
 	}
